c06544d66ee2b79b40c87972877a34939eb5df19
Phillip M Crandall <phillip.crandall@akebono-usa.com>
2019-04-16 18:58:12 -0500

    Initial commit

---
+++ helpers.c
@@ -0,0 +1,44 @@
+// Helper functions for music
+
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "helpers.h"
+
+
+/*duration, which should take as input as a string a fraction (e.g., 1/4)
+    and return as an int a corresponding number of eigths (2, in this case, since 1/4 is equivalent to 2/8)*/
+
+// Converts a fraction formatted as X/Y to eighths
+int duration(string fraction)
+{
+  
+}
+
+/*frequency, which should take as input as a string a note formatted as
+
+XY (e.g., A4), where X is any of A through G and Y is any of 0 through 8, or
+
+XYZ (e.g., A#4), where X is any of A through G, Y is # or b, and Z is any of 0 through 8,
+
+and return as an int the note’s corresponding frequency, rounded to the nearest integer; and
+
+
+// Calculates frequency (in Hz) of a note
+int frequency(string note)
+{
+    // TODO
+}
+
+is_rest, which should return true if its input,
+    a string, represents a rest in our machine-readable format, otherwise false.
+
+
+
+// Determines whether a string represents a rest
+bool is_rest(string s)
+{
+    // TODO
+}
+*/
